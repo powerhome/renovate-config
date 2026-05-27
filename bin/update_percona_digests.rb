8939908c00f4315efb6122ba3cd42fbde9c6355d
Colton Gerke <colton.gerke@powerhrg.com>
2026-05-27 09:42:28 -0500

    Refactor to use value objects where it makes sense

--- bin/update_percona_digests.rb
+++ bin/update_percona_digests.rb
@@ -8,21 +8,62 @@ require 'cgi'
 
 class PerconaDigestUpdater
   DIGEST_PATTERN = /\A[a-f0-9]{64}\z/i
-  SUPPORTED_ARCHITECTURES = ['amd64', 'x86_64', 'x86-64'].freeze
+
+  OperatorConfig = Struct.new(:name, :github_repo, :docs_base_url, :docs_pattern, :config_file, keyword_init: true) do
+    def release_notes_url(version)
+      "#{docs_base_url}/#{docs_pattern % version}"
+    end
+
+    def display_name
+      name.upcase
+    end
+  end
+
+  CertifiedImage = Struct.new(:package_name, :version, :digest, :architecture, keyword_init: true) do
+    SUPPORTED_ARCHITECTURES = ['amd64', 'x86_64', 'x86-64'].freeze
+
+    def supported_architecture?
+      architecture.nil? ||
+        architecture.strip.empty? ||
+        SUPPORTED_ARCHITECTURES.include?(architecture.downcase.strip)
+    end
+  end
+
+  ImageVersionSet = Struct.new(:package_name, :versions, keyword_init: true) do
+    def allowed_versions_pattern
+      version_list = versions.uniq.sort_by { |version| PerconaDigestUpdater.version_sort_key(version) }
+        .map { |version| Regexp.escape(version) }
+
+      "/^(#{version_list.join('|')})$/"
+    end
+  end
+
+  RenovatePackageRule = Struct.new(:image_version_set, keyword_init: true) do
+    def to_h
+      {
+        'matchDatasources' => ['docker'],
+        'matchPackageNames' => [image_version_set.package_name],
+        'allowedVersions' => image_version_set.allowed_versions_pattern,
+        'pinDigests' => true
+      }
+    end
+  end
 
   OPERATORS = {
-    'pxc' => {
+    'pxc' => OperatorConfig.new(
+      name: 'pxc',
       github_repo: 'percona/percona-xtradb-cluster-operator',
       docs_base_url: 'https://docs.percona.com/percona-operator-for-mysql/pxc/ReleaseNotes',
       docs_pattern: 'Kubernetes-Operator-for-PXC-RN%s.html',
       config_file: 'percona-pxc-versions.json'
-    },
-    'postgresql' => {
+    ),
+    'postgresql' => OperatorConfig.new(
+      name: 'postgresql',
       github_repo: 'percona/percona-postgresql-operator',
       docs_base_url: 'https://docs.percona.com/percona-operator-for-postgresql/latest/ReleaseNotes',
       docs_pattern: 'Kubernetes-Operator-for-PostgreSQL-RN%s.html',
       config_file: 'percona-postgresql-versions.json'
-    }
+    )
   }.freeze
 
   def initialize(operator = 'pxc', version = nil)
@@ -31,36 +72,38 @@ class PerconaDigestUpdater
     raise "Unsupported operator: #{@operator}. Supported: #{OPERATORS.keys.join(', ')}" unless @config
     
     @version = version || fetch_latest_version
-    @release_notes_url = "#{@config[:docs_base_url]}/#{@config[:docs_pattern] % @version}"
+    @release_notes_url = @config.release_notes_url(@version)
   end
 
   def run
-    puts "Processing Percona #{@operator.upcase} Operator v#{@version}"
+    puts "Processing Percona #{@config.display_name} Operator v#{@version}"
 
     release_content = fetch_release_notes
-    images = parse_certified_images(release_content)
+    certified_images = parse_certified_images(release_content)
 
-    if images.empty?
+    if certified_images.empty?
       puts "ERROR: No certified images found in release notes"
       exit 1
     end
 
-    puts "Found #{images.length} certified images:"
-    images.each { |name, versions| puts "  #{name}: #{versions.keys.join(', ')}" }
+    grouped_images = group_certified_images(certified_images)
 
-    update_renovate_config(images)
-    puts "Successfully updated #{@config[:config_file]}"
+    puts "Found #{grouped_images.length} certified images:"
+    grouped_images.each { |name, versions| puts "  #{name}: #{versions.keys.join(', ')}" }
 
-    images
+    update_renovate_config(certified_images)
+    puts "Successfully updated #{@config.config_file}"
+
+    certified_images
   end
 
   private
 
   def fetch_latest_version
-    github_api_url = "https://api.github.com/repos/#{@config[:github_repo]}/releases"
+    github_api_url = "https://api.github.com/repos/#{@config.github_repo}/releases"
     uri = URI(github_api_url)
     
-    puts "Fetching latest release from GitHub API for #{@config[:github_repo]}..."
+    puts "Fetching latest release from GitHub API for #{@config.github_repo}..."
     
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -123,7 +166,8 @@ class PerconaDigestUpdater
   end
 
   def parse_certified_images(html_content)
-    images = {}
+    certified_images = []
+    seen_images = {}
 
     # Parse HTML table rows for certified images
     # Look for pattern: <td>percona/image:version</td><td>digest</td>
@@ -144,20 +188,27 @@ class PerconaDigestUpdater
       # Parse image name and version
       match = image_cell.match(/\A(?<image_name>percona\/[^:]+):(?<version>.+?)(?:\s+\((?<architecture>[^)]+)\))?\z/)
       next unless match
-      next unless supported_architecture?(match[:architecture])
 
-      image_name = match[:image_name].strip
-      version = match[:version].strip
+      certified_image = CertifiedImage.new(
+        package_name: match[:image_name].strip,
+        version: match[:version].strip,
+        digest: digest_cell,
+        architecture: match[:architecture]
+      )
+      next unless certified_image.supported_architecture?
 
-      images[image_name] ||= {}
-      images[image_name][version] ||= digest_cell
+      image_key = [certified_image.package_name, certified_image.version]
+      next if seen_images[image_key]
+
+      certified_images << certified_image
+      seen_images[image_key] = true
     end
 
-    images
+    certified_images
   end
 
-  def update_renovate_config(images)
-    renovate_path = @config[:config_file]
+  def update_renovate_config(certified_images)
+    renovate_path = @config.config_file
 
     unless File.exist?(renovate_path)
       puts "ERROR: #{renovate_path} not found"
@@ -166,7 +217,7 @@ class PerconaDigestUpdater
 
     renovate_config = JSON.parse(File.read(renovate_path))
 
-    image_configs = build_image_configs(images)
+    image_version_sets = build_image_version_sets(certified_images)
 
     package_rules = renovate_config['packageRules'] || []
 
@@ -174,23 +225,18 @@ class PerconaDigestUpdater
       generated_image_rule?(rule)
     end
 
-    image_configs.each do |image_name, config|
-      package_rules << {
-        'matchDatasources' => ['docker'],
-        'matchPackageNames' => [image_name],
-        'allowedVersions' => config[:allowed_versions],
-        'pinDigests' => true
-      }
+    image_version_sets.each do |image_version_set|
+      package_rules << RenovatePackageRule.new(image_version_set: image_version_set).to_h
     end
 
     # Update the general Percona rule with all allowed versions
-    aggregate_allowed_versions = allowed_versions_pattern(image_configs.values.flat_map { |config| config[:versions] })
+    aggregate_allowed_versions = allowed_versions_pattern(image_version_sets.flat_map(&:versions))
 
     package_rules.each do |rule|
       next unless percona_aggregate_rule?(rule)
 
       rule['matchDatasources'] = ['docker']
-      rule['matchPackageNames'] = image_configs.keys.sort
+      rule['matchPackageNames'] = image_version_sets.map(&:package_name).sort
       rule['allowedVersions'] = aggregate_allowed_versions
       rule['pinDigests'] = true
     end
@@ -201,32 +247,25 @@ class PerconaDigestUpdater
     File.write(renovate_path, "#{JSON.pretty_generate(renovate_config)}\n")
   end
 
-  def build_image_configs(images)
-    configs = {}
-
-    images.each do |image_name, versions|
-      configs[image_name] = {
-        allowed_versions: allowed_versions_pattern(versions.keys),
-        versions: versions.keys
-      }
+  def build_image_version_sets(certified_images)
+    group_certified_images(certified_images).map do |image_name, versions|
+      ImageVersionSet.new(package_name: image_name, versions: versions.keys)
     end
+  end
 
-    configs
+  def group_certified_images(certified_images)
+    certified_images.each_with_object({}) do |certified_image, images|
+      images[certified_image.package_name] ||= {}
+      images[certified_image.package_name][certified_image.version] = certified_image.digest
+    end
   end
 
   def text_from_html(html)
     CGI.unescapeHTML(html.gsub(/<[^>]*>/, '')).strip
   end
 
-  def supported_architecture?(architecture)
-    return true if architecture.nil? || architecture.strip.empty?
-
-    SUPPORTED_ARCHITECTURES.include?(architecture.downcase.strip)
-  end
-
   def allowed_versions_pattern(versions)
-    version_list = versions.uniq.sort_by { |version| version_sort_key(version) }.map { |version| Regexp.escape(version) }
-    "/^(#{version_list.join('|')})$/"
+    ImageVersionSet.new(package_name: 'aggregate', versions: versions).allowed_versions_pattern
   end
 
   def percona_aggregate_rule?(rule)
@@ -245,7 +284,7 @@ class PerconaDigestUpdater
       !rule.key?('groupName')
   end
 
-  def version_sort_key(version)
+  def self.version_sort_key(version)
     parts = version.split(/[-.]/).map do |part|
       part.match?(/\A\d+\z/) ? part.to_i : part
     end
